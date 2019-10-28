2b7e4e930e9ecc49e4f31f4e4dae0e1f42b29bb2
Javed Wright <jdwright870@gmail.com>
2019-10-27 23:17:49 -0500

    Add set_message method

--- app/Emailer.py
+++ app/Emailer.py
@@ -1,14 +1,21 @@
 import smtplib
+import email
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class Emailer:
     __smtpObj = smtplib.SMTP()
     __user_details = dict()
-    __msg = dict()
+    __message = MIMEMultipart()
 
     def __init__(self, smtp_config, user_details, msg):
         self.__smtpObj = smtplib.SMTP(smtp_config['SMTP_SERVER'], smtp_config['SMTP_PORT'])
         self.__user_details = user_details
+        self.set_message(msg)
 
     def set_user_details(self, user_details):
         self.__user_details = user_details
@@ -21,3 +28,33 @@ class Emailer:
 
     def test_connection(self):
         return self.__smtpObj.ehlo()
+
+    def set_message(self, msg):
+        self.__message["From"] = msg['from']
+        self.__message["To"] = msg['to']
+        self.__message["Subject"] = msg['subject']
+        self.__message.attach(MIMEText(msg['body'], "plain"))
+
+        try:
+            with open(msg['attachment'], "rb") as attachment:
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
+        except FileNotFoundError:
+            print("File does not exist")
+        except IOError:
+            print("I/O Error")
+        except KeyError:
+            pass
+        except Exception as e:
+            print(e)
+
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {msg['attachment']}",
+        )
+
+        # Add attachment to message and convert message to string
+        self.__message.attach(part)
