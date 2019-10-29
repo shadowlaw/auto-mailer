12878f3a5f0bd10dfe4de0d57dc80aa287114f14
Javed Wright <wrightj@jncb.com>
2019-10-29 10:53:44 -0500

    Add vscode folder

--- test/EmailerTest.py
+++ test/EmailerTest.py
@@ -1,23 +1,28 @@
 import unittest
 from app.Emailer import Emailer
 from config.config import SMTP_CONFIG, EMAIL_CONFIG
+from os import getcwd
+from os import path
 
 
 class EmailerTest(unittest.TestCase):
 
     msg = dict()
+    emailer = Emailer()
 
     @classmethod
-    def setUpClass(cls):
-        cls.msg['to'] = ''
-        cls.msg['from'] = ''
-        cls.msg['subject'] = 'Test mail'
-        cls.msg['body'] = 'This is a test email'
-        cls.emailer = Emailer(SMTP_CONFIG, EMAIL_CONFIG, cls.msg)
+    def setUpClass(cls, self):
+        self.msg['to'] = ''
+        self.msg['from'] = ''
+        self.msg['subject'] = 'Test mail'
+        self.msg['body'] = 'This is a test email'
+        self.emailer = Emailer(SMTP_CONFIG, EMAIL_CONFIG, self.msg)
 
     def test_set_message_given_plaintext_message_and_pdf_attachment(self):
-        # TODO: Plain text email (success)
-        # TODO: email with attachment (success)
+        self.msg["attachment"] = path.join(getcwd(),"test_assets", "test.pdf")
+        self.emailer.set_message(self.msg)
+        emailer_message = self.emailer.get_message()
+        print(emailer_message)
         # TODO: Plain text email (failure)
         # TODO: email with attachment (failure)
         pass
