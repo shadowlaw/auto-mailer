40b99f221aa328a82e31b34f02997fa8e214f590
Javed Wright <jdwright870@gmail.com>
2019-10-30 22:29:04 -0500

    Change instance emailer to local

--- test/EmailerTest.py
+++ test/EmailerTest.py
@@ -7,7 +7,6 @@ from app.Emailer import Emailer
 from config.config import SMTP_CONFIG, EMAIL_CONFIG
 
 
-
 class EmailerTest(unittest.TestCase):
 
     msg = dict()
@@ -19,9 +18,12 @@ class EmailerTest(unittest.TestCase):
         cls.msg['from'] = ''
         cls.msg['subject'] = 'Test mail'
         cls.msg['body'] = 'This is a test email'
-        cls.emailer = Emailer(SMTP_CONFIG, EMAIL_CONFIG, cls.msg)
+        
 
     def setUp(self):
+        self.emailer = Emailer(SMTP_CONFIG, EMAIL_CONFIG, dict())
+
+    def tearDown(self):
         try:
             del self.msg["attachment"]
         except KeyError:
@@ -29,23 +31,26 @@ class EmailerTest(unittest.TestCase):
 
     def test_set_message_given_plaintext_message_and_pdf_attachment(self):
         msg = self.msg
+        emailer = self.emailer
         msg["attachment"] = path.join(getcwd(), "test", "test_assets", "test.pdf")
-        self.emailer.set_message(msg)
+        emailer.set_message(msg)
     
     def test_set_message_given_plaintext_message_and_incorrect_pdf_attachment_path(self):
         msg = self.msg
+        emailer = self.emailer
         msg["attachment"] = path.join(getcwd(), "test_assets", "test.pdf")
         
-        self.assertRaises(FileNotFoundError, self.emailer.set_message,msg)
+        self.assertRaises(FileNotFoundError, emailer.set_message, msg)
 
     def test_send_message_given_valid_mail_details(self):
         msg = self.msg
-        msg['to'] = ''
-        msg['from'] = ''
+        emailer = self.emailer
+        msg['to'] = "shadowdev03@gmail.com"
+        msg['from'] = "shadowdev03@gmail.com"
+        msg["body"] = "This is a test email"
         msg["attachment"] = path.join(getcwd(), "test", "test_assets", "test.pdf")
-        self.emailer.set_message(msg)
-        self.emailer.send_mail()
-        
+        emailer.set_message(msg)
+        emailer.send_mail()
 
 
 if __name__ == '__main__':
