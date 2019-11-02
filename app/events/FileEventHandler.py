682ef302d1dccc9232d3110f3b06cc0035f60496
Javed Wright <wrightj@jncb.com>
2019-11-02 16:39:37 -0500

    Add file event handler

---
+++ app/events/FileEventHandler.py
@@ -0,0 +1,21 @@
+import os
+
+from watchdog.events import RegexMatchingEventHandler
+
+class FileEventHandler(RegexMatchingEventHandler):
+    FILE_EXTS = [r"\d{4,4}-\d{2,2}\-\d{2,2}\-[A-Za-z]*\.pdf$"]
+
+    def __init__(self):
+        super().__init__(self.FILE_EXTS)
+
+    def on_created(self, event):
+        file_Size = -1
+        
+        while file_Size != os.path.getsize(event.src_path):
+            file_Size = os.path.getsize(event.src_path)
+            time.sleep(1)
+
+        self.process(event)
+
+    def process(self, event):
+        print(event.src_path)
