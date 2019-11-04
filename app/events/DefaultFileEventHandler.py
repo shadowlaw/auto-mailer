dc21b1598ec36e7ba3628e4df11544c4ec8de1f0
Javed Wright <jdwright870@gmail.com>
2019-11-03 21:01:05 -0500

    Implement process method

--- app/events/DefaultFileEventHandler.py
+++ app/events/DefaultFileEventHandler.py
@@ -1,6 +1,7 @@
 from .FileEventHandler import FileEventHandler
 from ..Emailer import Emailer
 from app.app import APP_CONFIG
+from json import load
 
 class DefaultFileEventHandler(FileEventHandler):
 
@@ -9,5 +10,30 @@ class DefaultFileEventHandler(FileEventHandler):
             self.regex = FILE_EXTS
         super().__init__()
 
-    def process(self):
-        pass
+    def process(self, event):
+
+        msg_data = dict()
+
+        try:
+            with open(APP_CONFIG["EMAIL_CONFIG"]["MAIL_DATA_PATH"],'r') as msg_fp:
+                msg_data = load(msg_fp)
+        except KeyError as e:
+            print("Unable to locate key: {}".format(e))
+            # send email with error message?
+            return
+        except FileNotFoundError as e:
+            print("Unable to file file: {}".format(e))
+            return
+        except IOError as e:
+            print("Unable to read file as location: {}".format(e))
+            return
+
+        msg_data["attachment"] = APP_CONFIG["EMAIL_CONFIG"]["MAIL_DATA_PATH"]
+        try:
+            emailer = Emailer(APP_CONFIG["SMTP_CONFIG"], APP_CONFIG["EMAIL_CONFIG"], msg_data)
+            emailer.send_mail()
+        except KeyError as e:
+            print("Unable to locate key: {}".format(e))
+        except Exception as e:
+            print("Emailer error: {}".format(e))
+        
