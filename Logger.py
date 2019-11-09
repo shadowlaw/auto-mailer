518b78fb7a6d34e683e2107f31fd19a0bd38f662
dannzii <34076867+dannzii@users.noreply.github.com>
2019-11-09 16:45:29 -0500

    So I added @property just try a few things out

--- Logger.py
+++ Logger.py
@@ -1,16 +1,26 @@
 import logging
 
+file_formatter = logging.Formatter("%(asctime)s:%(name)s:%(folder_name)s:%(loglevel)s:%(message)")
+
+file = logging.FileHandler('test.log')
+file.setFormatter(file_formatter)
+
 class Logger:
 
-    def __init__(self, name, folder, message_logged, loglevel=INFO, loggingtype=1):
+    def __init__(self, name, folder, message_logged, loglevel=logging.INFO, loggingtype=1):
         self.name = name
         self.folder_name = folder
         self.log_level = loglevel
         self.logging_type = loggingtype
         self.message = message_logged
 
-        logging.getLogger(format = "%(asctime)s:%(name)s:%(folder_name)s:%(loglevel)s:%(message)s")
+        this_log = logging.getLogger(__name__)
+        this_log.addHandler(file)
 
+    @property
     def __name__(self):
-        return logging.basicConfig(format="%(name)")
+        return '{}, {}, {}, {}, {}'.format(self.name, self.folder_name, self.log_level,self.logging_type, self.message, self.logging_type )
 
+
+emp_1 = Logger('John','testfile','Debug','Pizza delivered','1')
+emp_2 = Logger('Corey','testfile','Warning','Cheese','1')
