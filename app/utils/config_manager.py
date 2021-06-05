7abb6776db388c31b6f90f197317ca8ae759ad27
Javed Wright <jdwright870@gmail.com>
2021-06-05 18:24:39 -0500

    refactor config

---
+++ app/utils/config_manager.py
@@ -0,0 +1,31 @@
+from os import getcwd, path
+import logging
+
+from app.utils import file_manager
+
+
+def __getUserConfig():
+    return __read_config_file(path.join(getcwd(), "app/config/user_conf.yaml"))
+
+
+def __getSystemConfig():
+    sys_config = __read_config_file(path.join(getcwd(), "app/config/system_conf.yaml"))
+    __prepareLogging(sys_config)
+    return sys_config
+
+
+def __prepareLogging(sys_conf):
+    sys_conf['LOGGING_CONFIG']['DEFAULT_LOG_LEVEL'] = getattr(logging, sys_conf['LOGGING_CONFIG'][
+        'DEFAULT_LOG_LEVEL'].upper(), 20)
+
+    file_manager.create_file(sys_conf['LOGGING_CONFIG']['LOG_LOCATION'])
+
+
+def __read_config_file(config_path):
+    return file_manager.read_yaml_file(path.normpath(config_path))
+
+
+def getAppConfig():
+    app_config = __getUserConfig()
+    app_config.update(__getSystemConfig())
+    return app_config
--- app/utils/file_manager.py
+++ app/utils/file_manager.py
@@ -1,3 +1,4 @@
+from os import path, mkdir
 import yaml
 from json import load
 
@@ -10,3 +11,11 @@ def read_yaml_file(path):
 def read_json_file(path):
     with open(path, 'r') as fptr:
         return load(fptr)
+
+
+def create_file(file_path):
+    if not path.exists(file_path):
+        if not path.exists(path.dirname(file_path)):
+            mkdir(path.dirname(file_path))
+        with open(file_path, 'w'):
+            pass
